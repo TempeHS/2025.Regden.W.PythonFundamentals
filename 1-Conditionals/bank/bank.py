6bd4b74a04dd264767044f9b15fad979b6e44176
Icymonkey-guy <158523727+Icymonkey-guy@users.noreply.github.com>
2024-02-12 00:35:13 +0000

    completed bank.py

--- 1-Conditionals/bank/bank.py
+++ 1-Conditionals/bank/bank.py
@@ -1,22 +1,7 @@
-# if hello print $0
-# elif h or no h boolean
-# True print $20
-# false Print $100
-
-
-# .strip whitesapce
-# .startswith for hello
-# also .find for hello
-# firstcharacter = str[:1]
-
 greeting = input("How would you like to be greeted? ")
-if greeting.startswith("hello"):
+if greeting.strip(" ").lower().startswith("hello"):
     print("$0")
-elif greeting.startswith("Hello"):
-    print("$20")
-elif greeting.startswith("h"):
-    print("$20")
-elif greeting.startswith("H"):
+elif greeting.strip(" ").lower().startswith("h"):
     print("$20")
 else:
     print("$100")
