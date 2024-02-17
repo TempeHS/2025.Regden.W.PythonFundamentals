60747a8c4ae436dad41533b312c18a3956244acc
Icymonkey-guy <158523727+Icymonkey-guy@users.noreply.github.com>
2024-02-17 00:40:35 +0000

    completed meal.py

--- 1-Conditionals/meal/meal.py
+++ 1-Conditionals/meal/meal.py
@@ -2,20 +2,21 @@ def convert(time: str):
     hours, minutes = time.split(":")
     x = float(minutes) / 60
     z = float(hours) + x
+    if 7.0 <= z <= 8.0:
+        print("it is breakfast time")
+    elif 12.0 < z <= 13.0:
+        print("it is lunch time")
+    elif 18.0 < z <= 19.0:
+        print("it is dinner time")
+    else:
+        print("it is not time to eat")
     return z
 
 
 def main():
     time = input("What is the time? ")
-    y = convert(time)
+    time = convert(time)
 
-
-if 7.0 <= y <= 8.0:
-    print("it is breakfast time")
-elif 8.0 < y < +9.0:
-    print("x is greater than y")
-elif 9.0 < y <= 10.0:
-    print("x is equal to y")
 
 if __name__ == "__main__":
     main()
