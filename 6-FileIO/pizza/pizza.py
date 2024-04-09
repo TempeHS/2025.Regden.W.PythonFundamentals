362e3f9625101aec4b07f2d694fff2345baf88bb
Icymonkey-guy <158523727+Icymonkey-guy@users.noreply.github.com>
2024-04-09 08:05:41 +0000

    finished pizza.py

---
+++ 6-FileIO/pizza/pizza.py
@@ -0,0 +1,32 @@
+import sys
+from tabulate import tabulate
+
+if len(sys.argv) < 2:
+    print("Too few arguments")
+    sys.exit
+
+
+elif len(sys.argv) > 2:
+    print("Too many arguments")
+    sys.exit
+
+elif not (sys.argv[1]).endswith(".csv"):
+    print("Not a csv file")
+    sys.exit
+
+else:
+    data = []
+    try:
+        with open(sys.argv[1]) as file:
+            for line in file:
+                data.append(line.strip().split(","))
+    except FileNotFoundError:
+        print("file does not exist")
+
+    print(
+        tabulate(
+            data,
+            headers="firstrow",
+            tablefmt="grid",
+        )
+    )
